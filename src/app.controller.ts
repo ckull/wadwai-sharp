baa6065f8fa2ba8eb8797c558cf78669a3a66bae
ksiri <ksiri@sertiscorp.com>
2023-07-07 12:11:35 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,10 @@ import { AppService } from './app.service';
 import * as sharp from 'sharp'
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response, response } from 'express';
-
+import { rgb2cmyk } from './utils';
+import * as gm from 'gm'
+import { buffer } from 'stream/consumers';
+import { error } from 'console';
 
 @Controller()
 export class AppController {
@@ -21,13 +24,48 @@ export class AppController {
   ) {
     try {
       console.log(file);
-  
+
       const metadata = await sharp(file.buffer).metadata();
-  
+
+      console.log('metadata: ', metadata)
       return metadata;
     } catch (error) {
       console.error('Error extracting metadata:', error);
       throw new Error('Failed to extract metadata');
     }
   }
+
+  // @ts-ignore
+  // @Post('/process')
+  // @UseInterceptors(FileInterceptor('file'))
+  // public async process(
+  //   @UploadedFile() file: Express.Multer.File,
+  //   @Res() res: Response,
+  // ) {
+  //   try {
+  //     const profilePath = 'USWebCoatedSWOP.icc'
+
+  //     gm.subClass({ imageMagick: true });
+
+  //     gm(file.buffer)
+  //     .profile(profilePath)
+  //     .toBuffer('JPEG', (err, imageBuffer) => {
+  //       if(err) {
+  //         throw new Error(err)
+  //       }
+
+  //       console.log('imageBuffer: ', imageBuffer)
+  //       const base64Image = imageBuffer.toString('base64')
+
+  //       const dataURL = `data:image/png;base64,${base64Image}`;
+  //        res.setHeader('Content-Type', 'application/json');
+  //        res.end(JSON.stringify({dataURL}));
+  //     })
+
+    
+  //   } catch (error) {
+  //     console.error('Error extracting metadata:', error);
+  //     throw new Error('Failed to extract metadata');
+  //   }
+  // }
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,30 @@
+// @ts-ignore
+export const rgb2cmyk = function (r, g, b, normalized) {
+    var c = 1 - (r / 255);
+    var m = 1 - (g / 255);
+    var y = 1 - (b / 255);
+    var k = Math.min(c, Math.min(m, y));
+  
+    c = (c - k) / (1 - k);
+    m = (m - k) / (1 - k);
+    y = (y - k) / (1 - k);
+  
+    if (!normalized) {
+      c = Math.round(c * 10000) / 100;
+      m = Math.round(m * 10000) / 100;
+      y = Math.round(y * 10000) / 100;
+      k = Math.round(k * 10000) / 100;
+    }
+  
+    c = isNaN(c) ? 0 : c;
+    m = isNaN(m) ? 0 : m;
+    y = isNaN(y) ? 0 : y;
+    k = isNaN(k) ? 0 : k;
+  
+    return {
+      c: c,
+      m: m,
+      y: y,
+      k: k,
+    };
+  };
