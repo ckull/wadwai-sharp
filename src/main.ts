eb114b7a5f0748eaf48155f88b0eeb294a171b55
ksiri <ksiri@sertiscorp.com>
2023-08-15 15:44:11 +0700

    add adminConfig

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
-import { adminConfig } from 'secret/wadwai-official-firebase-adminsdk-49ow7-a39fcd7be2';
+import { adminConfig } from 'wadwai-official-firebase-adminsdk-49ow7-a39fcd7be2';
 import { ServiceAccount } from 'firebase-admin';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   admin.initializeApp({
-    credential: admin.credential.cert(
-      process.env.FIREBASE_ADMIN as ServiceAccount,
-    ),
+    credential: admin.credential.cert(adminConfig as ServiceAccount),
   });
 
   await app.listen(3001);
