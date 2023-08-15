051c8ee1af6f35000f7f094b5b20aa46a03ed033
ksiri <ksiri@sertiscorp.com>
2023-08-15 16:02:35 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
-import { adminConfig } from 'etc/secrets/wadwai-official-firebase-adminsdk-49ow7-a39fcd7be2';
+import { adminConfig } from '../etc/secrets/wadwai-official-firebase-adminsdk-49ow7-a39fcd7be2';
 import { ServiceAccount } from 'firebase-admin';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
